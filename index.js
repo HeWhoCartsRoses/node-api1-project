6d72590de87e31ffd27f9857d17c35fe558df343
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-03-10 15:38:11 -0600

    confusing garbagio

--- index.js
+++ index.js
@@ -13,18 +13,29 @@ let temp = [
     bio: "Not Tarzan's Wife, another Jane" // String, required
   }
 ];
+let id = "a_unique_id";
 const port = 5000;
 const key = "/api/users";
 server.get(key, (req, res) => {
-  res.status(200).json(temp);
+  if (req != null) res.status(200).json(temp);
+  else res.status(404);
 });
 server.post(key, (req, res) => {
   const info = req.body;
-  info.id = shortid.generate();
-  temp.push(info);
-  res.status(201).json(temp);
+  if (info.name === undefined || info.bio === undefined) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    info.id = shortid.generate();
+    temp.push(info);
+    res.status(201).json(temp);
+  }
 });
-
 server.listen(port, () =>
   console.log(`\n**API on http://localhost:${port}**\n`)
 );
+//for getting the id, i think(?)
+// server.get(key+id, (req,res)=>{
+
+// })
